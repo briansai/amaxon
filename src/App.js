b0a247ff5c44e875c73272b373a87e9eed5ae34a
Brian Sai <brianwsai@gmail.com>
2021-05-23 15:29:42 -0700

    Created react app and implemented firebase file

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello</h1>
+      {/* Header */}
+      {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAt99lSo6IZjj6PafXdX9WYTglmgkFGV4Q',
+  authDomain: 'ian-9a6ef.firebaseapp.com',
+  projectId: 'ian-9a6ef',
+  storageBucket: 'ian-9a6ef.appspot.com',
+  messagingSenderId: '782053154065',
+  appId: '1:782053154065:web:4ff43f6e4168de19b880b0',
+};
