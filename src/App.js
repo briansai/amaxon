da8a8e9f2e7c10d0085455513dbee92520dd9240
Brian Sai <brianwsai@gmail.com>
2021-05-24 16:44:51 -0700

    Implemented React Router to manage routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
+import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path="/checkout">
+            <h1>I AM A CHECKOUT, SMASTH THE LIKE BUTTON</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
