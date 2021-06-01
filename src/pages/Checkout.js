147f8d6c19e84543c38d764ec7efe460a0c9bbb7
Brian Sai <brianwsai@gmail.com>
2021-06-01 13:30:42 -0700

    Put logic for checkout page to display email if user is signed on

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -5,7 +5,7 @@ import Subtotal from '../components/Subtotal';
 import './Checkout.css';
 
 function Checkout() {
-  const [{ cart }] = useStateValue();
+  const [{ cart, user }] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -15,6 +15,7 @@ function Checkout() {
           alt=""
         />
         <div>
+          <h3>Hello, {user?.email || 'Guest'}</h3>
           <h2 className="checkout__title">Your Shopping Cart</h2>
           {cart.map((item, index) => (
             <CheckoutProduct
