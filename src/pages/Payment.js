90774f8bb80d1eb7c0dcc17bf890ebd22f064aeb
Brian Sai <brianwsai@gmail.com>
2021-06-02 17:06:03 -0700

    Listed items on payment page and put fake delivery address as placeholder

--- src/pages/Payment.js
+++ src/pages/Payment.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useStateValue } from '../context/StateProvider';
+import CheckoutProduct from '../components/CheckoutProduct';
 import './Payment.css';
 
 function Payment() {
+  const [{ cart, user }, dispatch] = useStateValue();
+
   return (
     <div className="payment">
-      <div className="payment__container"></div>
+      <div className="payment__container">
+        <h1>Checkout {<Link to="/checkout"> ({cart.length}) items </Link>}</h1>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Deliver Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>123 Potato Lane</p>
+            <p>Los Angeles, CA</p>
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {cart.map((item) => (
+              <CheckoutProduct item={item} />
+            ))}
+          </div>
+        </div>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details"></div>
+        </div>
+      </div>
     </div>
   );
 }
