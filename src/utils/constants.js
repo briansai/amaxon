97a0dab118c66e874d50dcfd1a022eb1076245f6
Brian Sai <brianwsai@gmail.com>
2021-06-06 16:34:37 -0700

    Included link to orders in header

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -8,7 +8,7 @@ export const headerOptions = [
     line2: 'Sign In',
     link: '/login',
   },
-  { line1: 'Returns', line2: '& Orders', link: '' },
+  { line1: 'Returns', line2: '& Orders', link: '/orders' },
   { line1: 'Your', line2: 'Prime', link: '' },
 ];
 
